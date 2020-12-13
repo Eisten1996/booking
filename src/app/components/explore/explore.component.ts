4a679b9544738cba824413ce24b148d9dfbdc01c
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-12-13 15:34:36 -0500

    Declaramos nuestro service

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/services/app.service';
+import { RestaurantLight } from 'src/app/shared/models/restaurant-light-models';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.scss']
+  styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
+  restaurants: RestaurantLight[];
 
-  constructor() { }
+  constructor(private appService: AppService) {}
 
   ngOnInit(): void {
+    this.appService.getAllRestaurantsMock();
   }
-
 }
