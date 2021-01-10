60ee00d1a2fdb60ef6fae68a801eff3aa1d6fa1a
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-01-10 00:19:11 -0500

    Formulario Stripe (II)

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { StripeService, StripeCardComponent } from 'ngx-stripe';
+import {
+  StripeCardElementOptions,
+  StripeElementsOptions,
+} from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-payment',
@@ -6,11 +13,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent implements OnInit {
-  constructor() {}
+  @ViewChild(StripeCardComponent) card: StripeCardComponent;
 
-  ngOnInit(): void {}
+  cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        iconColor: '#666EE8',
+        color: '#31325F',
+        fontWeight: '300',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSize: '18px',
+        '::placeholder': {
+          color: '#CFD7E0',
+        },
+      },
+    },
+  };
+
+  elementsOptions: StripeElementsOptions = {
+    locale: 'es',
+  };
+
+  stripeTest: FormGroup;
+
+  constructor(private fb: FormBuilder, private stripeService: StripeService) {}
+
+  ngOnInit(): void {
+    this.stripeTest = this.fb.group({
+      name: ['', [Validators.required]],
+    });
+  }
 
   cancel() {}
 
   confirm() {}
+
+  buy() {}
 }
